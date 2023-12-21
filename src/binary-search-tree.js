929737db95139374329aa54d13e02887fc768b70
ayzana <aizanka001@gmail.com>
2023-12-21 17:38:53 +0300

    feat: add solution

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,32 +1,68 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+//const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+class Node {
+
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
 class BinarySearchTree {
-
+  constructor()
+  {
+     this.rootel = null;
+  }
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.rootel)
+    { return null;}
+
+    return this.rootel;
+
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+  add(data) {
+    this.rootel = addData(this.rootel, data);
+    function addData(node, data){
+      if (!node){
+        return new Node(data);
+      }
+      if (node.data === data){
+        return node;
+      }
+      if (data < node.data){
+        node.left = addData(node.left, data);
+      } else {
+        node.right = addData(node.right, data);
+      }
+      return node;
+    }
+}
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has( data) {
+    return hasData(this.rootel, data);
+    function hasData(node, data){
+      if (!node){
+        return false;
+      }
+      if (node.data == data){
+        return true;
+      }
+      return data < node.data ? hasData(node.left, data):hasData(node.right, data);
+    }
   }
 
   find(/* data */) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
+
 
   remove(/* data */) {
     throw new NotImplementedError('Not implemented');
